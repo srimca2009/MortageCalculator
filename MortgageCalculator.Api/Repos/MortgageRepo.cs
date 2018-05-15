3aa706490365f7e48382e6f8a7fb961eeb5c18d6
sritharan <srimca2009@gmail.com>
2018-05-15 11:58:17 +0530

    Added memory cache

--- MortgageCalculator.Api/Repos/MortgageRepo.cs
+++ MortgageCalculator.Api/Repos/MortgageRepo.cs
@@ -1,18 +1,40 @@
-﻿using System;
+﻿using MortgageCalculator.Dto;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using MortgageCalculator.Dto;
+using System.Runtime.Caching;
 
 namespace MortgageCalculator.Api.Repos
 {
     public interface IMortgageRepo
     {
         List<Mortgage> GetAllMortgages();
+        IList GetAll();
     }
 
     public class MortgageRepo : IMortgageRepo
     {
+        private const string CacheKey = "mortage";
+        public IList GetAll()
+        {
+            ObjectCache cache = MemoryCache.Default;
+
+            if (cache.Contains(CacheKey))
+                return (IList)cache.Get(CacheKey);
+            else
+            {
+                IList getAllMortgages = this.GetAllMortgages();
+
+                // Store data in the cache
+                CacheItemPolicy cacheItemPolicy = new CacheItemPolicy();
+                cacheItemPolicy.AbsoluteExpiration = DateTime.Now.AddHours(1.0);
+                cache.Add(CacheKey, getAllMortgages, cacheItemPolicy);
+
+                return getAllMortgages;
+            }
+        }
+       
         public List<Mortgage> GetAllMortgages()
         {
             using (var context = new MortgageData.MortgageDataContext())
@@ -30,7 +52,7 @@ namespace MortgageCalculator.Api.Repos
                             EstablishmentFee = mortgage.CancellationFee,
                             InterestRepayment = (InterestRepayment)Enum.Parse(typeof(InterestRepayment), mortgage.MortgageType.ToString()),
                             MortgageId = mortgage.MortgageId,
-                           MortgageType = (MortgageType)Enum.Parse(typeof(MortgageType), mortgage.MortgageType.ToString()),
+                            MortgageType = (MortgageType)Enum.Parse(typeof(MortgageType), mortgage.MortgageType.ToString()),
                            //TermsInMonths = mortgage.TermsInMonths
                         }
                     );
