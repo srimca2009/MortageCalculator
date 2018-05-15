6701305d5863a7bd6f7c9bcf96f21765b3dd2ec5
sritharan <srimca2009@gmail.com>
2018-05-15 09:45:41 +0530

    First Commit

---
+++ MortgageCalculator.Api/Controllers/MortgageController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using MortgageCalculator.Api.Services;
+
+namespace MortgageCalculator.Api.Controllers
+{
+    public class MortgageController : ApiController
+    {
+        // GET: api/Mortgage
+        public IEnumerable<Dto.Mortgage> Get()
+        {
+           var mortgageService = new MortgageService();
+            return mortgageService.GetAllMortgages();
+        }
+
+        // GET: api/Mortgage/5
+        public Dto.Mortgage Get(int id)
+        {
+            var mortgageService = new MortgageService();
+            return mortgageService.GetAllMortgages().FirstOrDefault(x => x.MortgageId == id);
+        }
+    }
+}
