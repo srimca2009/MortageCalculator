79f593d7b30b3dc22573acae745aa92e64cb67bc
Sri <Sri@DESKTOP-M6UPKFT>
2018-05-21 08:23:28 +0530

    Added Repository and service test case.

---
+++ MortgageCalculator.UnitTests/Repository/MortgageRepositoryTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MortgageCalculator.Repository;
+using System.Linq;
+
+namespace MortgageCalculator.UnitTests
+{
+    [TestClass]
+    public class MortgageRepositoryTest
+    {
+        MortgageData.MortgageDataContext _databaseContext;
+        MortgageRepository _objRepo;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _databaseContext = new MortgageData.MortgageDataContext();
+            _objRepo = new MortgageRepository();
+
+        }
+
+        [TestMethod]
+        public void Mortgage_Repository_Get_ALL()
+        {
+            var result = _objRepo.GetAll().ToList();
+            Assert.IsNotNull(result);
+            Assert.AreEqual(8, result.Count);
+        }
+
+
+        [TestMethod]
+        public void Mortgage_Repository_Get_ALL_NotEqual()
+        {
+            //Act
+            var result = _objRepo.GetAll().ToList();
+            Assert.AreNotEqual(9, result.Count);
+        }
+    }
+}
---
+++ MortgageCalculator.UnitTests/Service/MortgageServiceTest.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MortgageCalculator.Dto;
+using MortgageCalculator.Repository;
+using MortgageCalculator.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortgageCalculator.UnitTests
+{
+    [TestClass]
+    public class MortgageServiceTest
+    {
+
+        private Mock<IMortgageRepository> _mockRepository;
+        private IMortgageService _service;
+        List<Mortgage> _listMortgages;
+        Mortgage _mortgage;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mockRepository = new Mock<IMortgageRepository>();
+            _service = new MortgageService(_mockRepository.Object);
+
+            _listMortgages = new List<Mortgage>() {
+             new Mortgage() {
+                 MortgageId =1, Name="Test",MortgageType=MortgageType.Fixed, InterestRepayment = InterestRepayment.InterestOnly, EffectiveStartDate= DateTime.Now.AddDays(-365),EffectiveEndDate=DateTime.Now, TermsInMonths=12, CancellationFee=0,EstablishmentFee=1000, InterestRate=13.5M },
+                         new Mortgage() {
+                 MortgageId =2, Name="TestOne",MortgageType=MortgageType.Variable, InterestRepayment = InterestRepayment.InterestOnly, EffectiveStartDate= DateTime.Now.AddDays(-365),EffectiveEndDate=DateTime.Now, TermsInMonths=12, CancellationFee=0,EstablishmentFee=1000, InterestRate=13.5M },
+            };
+
+
+            _mortgage = new Mortgage()
+            {
+                MortgageId = 1,
+                Name = "Test",
+                MortgageType = MortgageType.Fixed,
+                InterestRepayment = InterestRepayment.InterestOnly,
+                EffectiveStartDate = DateTime.Now.AddDays(-365),
+                EffectiveEndDate = DateTime.Now,
+                TermsInMonths = 12,
+                CancellationFee = 0,
+                EstablishmentFee = 1000,
+                InterestRate = 13.5M
+            };
+
+        }
+
+        [TestMethod]
+        public void Mortgage_Get_All()
+        {
+            _mockRepository.Setup(x => x.GetAll()).Returns(_listMortgages);
+            List<Mortgage> results = _service.GetAllMortgages() as List<Mortgage>;
+            Assert.IsNotNull(results);
+            Assert.AreEqual(2, results.Count);
+        }
+
+        [TestMethod]
+        public void Mortgage_Get_All_NotEqual()
+        {
+            _mockRepository.Setup(x => x.GetAll()).Returns(_listMortgages);
+            List<Mortgage> results = _service.GetAllMortgages() as List<Mortgage>;
+            Assert.AreNotEqual(4, results.Count);
+        }
+
+
+        [TestMethod]
+        public void Mortgage_Get()
+        {
+            _mockRepository.Setup(x => x.GetAll()).Returns(_listMortgages);
+            Mortgage results = _service.GetbyId(1) as Mortgage;
+            Assert.AreEqual(1, results.MortgageId);
+        }
+
+        [TestMethod]
+        public void Mortgage_Get_NotEqual()
+        {
+            _mockRepository.Setup(x => x.GetAll()).Returns(_listMortgages);
+            Mortgage results = _service.GetbyId(1) as Mortgage;
+            Assert.AreNotEqual(2, results.MortgageId);
+        }
+    }
+}
